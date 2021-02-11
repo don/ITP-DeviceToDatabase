e8431c2b8e853544520e84fb6e5aaf2261f19f2e
Don Coleman <don.coleman@gmail.com>
2021-02-11 13:18:05 -0500

    2021 data

--- 03_RelationalDatabases/person_device_schema.sql
+++ 03_RelationalDatabases/person_device_schema.sql
@@ -16,7 +16,7 @@ CREATE TABLE device (
     FOREIGN KEY (person_id) REFERENCES person(id)
 );
 
--- tell sqlite to enforce foreing keys
+-- tell sqlite to enforce foreign keys
 PRAGMA foreign_keys=1;
 
 -- -- PostgreSQL
@@ -34,37 +34,40 @@ PRAGMA foreign_keys=1;
 -- );
 
 -- Data
-INSERT INTO person (id, name) VALUES (1, 'Don');
-INSERT INTO device (name, person_id) VALUES ('device_00', 1);
-INSERT INTO person (id, name) VALUES (2, 'Neta');
-INSERT INTO device (name, person_id) VALUES ('device_10', 2);
-INSERT INTO person (id, name) VALUES (3, 'KJ');
-INSERT INTO device (name, person_id) VALUES ('device_11', 3);
-INSERT INTO person (id, name) VALUES (4, 'Aditya');
-INSERT INTO device (name, person_id) VALUES ('device_12', 4);
-INSERT INTO person (id, name) VALUES (5, 'Nikhil');
-INSERT INTO device (name, person_id) VALUES ('device_13', 5);
-INSERT INTO person (id, name) VALUES (6, 'Yongkun');
-INSERT INTO device (name, person_id) VALUES ('device_14', 6);
-INSERT INTO person (id, name) VALUES (7, 'Martin');
-INSERT INTO device (name, person_id) VALUES ('device_15', 7);
-INSERT INTO person (id, name) VALUES (8, 'Erkin');
-INSERT INTO device (name, person_id) VALUES ('device_16', 8);
-INSERT INTO person (id, name) VALUES (9, 'Beste');
-INSERT INTO device (name, person_id) VALUES ('device_17', 9);
-INSERT INTO person (id, name) VALUES (10, 'Bilal');
-INSERT INTO device (name, person_id) VALUES ('device_18', 10);
-INSERT INTO person (id, name) VALUES (11, 'Jake');
-INSERT INTO device (name, person_id) VALUES ('device_19', 11);
-INSERT INTO person (id, name) VALUES (12, 'Yunze');
-INSERT INTO device (name, person_id) VALUES ('device_20', 12);
-INSERT INTO person (id, name) VALUES (13, 'Stacy');
-INSERT INTO device (name, person_id) VALUES ('device_22', 13);
-INSERT INTO person (id, name) VALUES (14, 'Helen');
-INSERT INTO device (name, person_id) VALUES ('device_23', 14);
+INSERT INTO person (id, name) VALUES (1, 'Jingxi');
+INSERT INTO device (name, person_id) VALUES ('device_10', 1);
+INSERT INTO person (id, name) VALUES (2, 'Justin');
+INSERT INTO device (name, person_id) VALUES ('device_11', 2);
+INSERT INTO person (id, name) VALUES (3, 'Wasif');
+INSERT INTO device (name, person_id) VALUES ('device_12', 3);
+INSERT INTO person (id, name) VALUES (4, 'Name');
+INSERT INTO device (name, person_id) VALUES ('device_14', 4);
+INSERT INTO person (id, name) VALUES (5, 'Kyungah');
+INSERT INTO device (name, person_id) VALUES ('device_15', 5);
+INSERT INTO person (id, name) VALUES (6, 'Abby');
+INSERT INTO device (name, person_id) VALUES ('device_16', 6);
+INSERT INTO person (id, name) VALUES (7, 'Paulami');
+INSERT INTO device (name, person_id) VALUES ('device_18', 7);
+INSERT INTO person (id, name) VALUES (8, 'Yonatan');
+INSERT INTO device (name, person_id) VALUES ('device_19', 8);
+INSERT INTO person (id, name) VALUES (9, 'Tianjun');
+INSERT INTO device (name, person_id) VALUES ('device_20', 9);
+INSERT INTO person (id, name) VALUES (10, 'Alan');
+INSERT INTO device (name, person_id) VALUES ('device_21', 10);
+INSERT INTO person (id, name) VALUES (11, 'Youming');
+INSERT INTO device (name, person_id) VALUES ('device_23', 11);
+INSERT INTO person (id, name) VALUES (12, 'Lanni');
+INSERT INTO device (name, person_id) VALUES ('device_24', 12);
+INSERT INTO person (id, name) VALUES (13, 'Yilin');
+INSERT INTO device (name, person_id) VALUES ('device_25', 13);
+INSERT INTO person (id, name) VALUES (14, 'Don');
+INSERT INTO device (name, person_id) VALUES ('device_00', 14);
 -- extra devices
+INSERT INTO device (name, person_id) SELECT 'machine', id FROM person WHERE name = 'Don';
 INSERT INTO device (name, person_id) SELECT 'device_01', id FROM person WHERE name = 'Don';
 INSERT INTO device (name, person_id) SELECT 'device_02', id FROM person WHERE name = 'Don';
 INSERT INTO device (name, person_id) SELECT 'device_03', id FROM person WHERE name = 'Don';
 INSERT INTO device (name, person_id) SELECT 'device_04', id FROM person WHERE name = 'Don';
-INSERT INTO device (name, person_id) SELECT 'device_24', id FROM person WHERE name = 'Bilal';
+INSERT INTO device (name, person_id) SELECT 'device_05', id FROM person WHERE name = 'Don';
+INSERT INTO device (name, person_id) SELECT 'device_27', id FROM person WHERE name = 'Jingxi';
+
